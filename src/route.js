e813ead7f88b4eff79a2a1ac6c5ecc75fe1e460b
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2021-06-24 19:46:10 -0300

    day 4

---
+++ src/route.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => {
+    res.render("index")
+})
+
+route.get('/room', (req, res) =>{
+    res.render("room")
+})
+
+route.get('/create-pass', (req, res) =>{
+    res.render("create-pass")
+})
+
+// route.post("room/:room/:question/:action")
+
+
+module.exports = route
+
+// 1:59:40
